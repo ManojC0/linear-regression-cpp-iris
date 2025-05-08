7bf7542a302aa50be7c2cc713350b0479cbbf3ff
ManojC0 <94547054+ManojC0@users.noreply.github.com>
2025-05-08 18:33:16 +0100

    Cleanup

--- main.cpp
+++ main.cpp
@@ -36,21 +36,21 @@ std::istream& operator>>(std::istream& input, Data& df)
 
 
 // Gradient descent function
-std::tuple<float,float> gradient_descent(std::vector<Data>& df, float m, float b, double L) {
+std::tuple<float,float> gradient_descent(const std::vector<Data>& df, float m, float b, double L) {
 	float m_gradient{0};
 	float b_gradient{0};
+	const int df_size = df.size();
 
-	for (unsigned i = 0; i < df.size(); i++) {
+	for (unsigned i = 0; i < df_size; i++) {
 		float x = df[i].sepalLength;
 		float y = df[i].petalLength;
 
 		m_gradient += x * (y - (m * x + b));
-		b_gradient +=  (y - (m * x + b));
-	
+		b_gradient +=  y - (m * x + b);
 	}
 	
-	float new_m = m - ((-2.0f) / df.size()) * m_gradient * L;
-	float new_b = b - ((-2.0f) / df.size()) * b_gradient * L;
+	float new_m = m - (-2.0f / df_size) * m_gradient * L;
+	float new_b = b - (-2.0f / df_size) * b_gradient * L;
 	return { new_m, new_b };
 }
 
@@ -76,8 +76,8 @@ int main() {
 
 	float m = 0;
 	float b = 0;
-	double learning_rate{ 0.0001 };
-	int epochs = 1000;
+	const double learning_rate{ 0.0001 };
+	const int epochs = 5000;
 
 	std::tuple<float, float> vals{};
 
@@ -90,14 +90,10 @@ int main() {
 		if (i % 100 == 0) {
 			std::cout << "Epoch: " << i << "\n";
 		}
-
-
 	}
 
 	std::cout << "Final m: " << m << '\n';
 	std::cout << "Final b: " << b;
 	
-
 	return 0;
-
 }
